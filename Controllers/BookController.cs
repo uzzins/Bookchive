06e5bc57572ee0a98084acdfc4dff660e926c732
uzinsea@gmail.com <uzinsea@gmail.com>
2025-02-17 22:18:08 +0900

    초기 커밋.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookchive.Controllers
+{
+    [Route("Book")]
+    public class BookController : Controller
+    {
+        private const string _viewPath = "~/Pages/Book";
+
+        [Route("List")]
+        public ActionResult List()
+        {
+            return View($"{_viewPath}/Index",null);
+        }
+
+
+        [Route("register")]
+        public ActionResult Register()
+        {
+            return View($"{_viewPath}/Register",null);
+        }
+    }
+}
